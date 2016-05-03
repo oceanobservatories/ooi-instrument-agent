db94dbeb11fc6e50697b2b601e6adec59caa56e5
Peter Cable <petercable@gmail.com>
2016-05-03 09:20:01 -0700

    Initial commit
    Change-Id: Ia446de07039e13e2dfae6cc4c8e4c61881c459d6

---
+++ ooi_instrument_agent/client.py
@@ -0,0 +1,92 @@
+import logging
+
+import zmq.green as zmq
+
+
+log = logging.getLogger(__name__)
+context = zmq.Context()
+
+
+class TimeoutException(Exception):
+    status_code = 408
+
+    def __init__(self, message=None):
+        Exception.__init__(self)
+        self.message = message
+
+
+class ZmqDriverClient(object):
+    """
+    A class for performing RPC with a ZMQ-based driver process
+
+    Utilizes zmq.green to provide a gevent-compatible version of zmq
+    """
+    def __init__(self, host, port):
+        """
+        :param host: Hostname or IP of the target driver
+        :param port: Port number of the target driver
+        """
+        self.host = host
+        self.port = port
+        log.debug('Start %r', self)
+
+    def _connect(self):
+        """
+        :return: Connected ZMQ REQ socket
+        """
+        log.debug('Connecting ZMQ client: %r', self)
+        socket = context.socket(zmq.REQ)
+        socket.connect('tcp://{host}:{port}'.format(host=self.host, port=self.port))
+        log.debug('Connected: %r', self)
+        return socket
+
+    def _command(self, command, *args, **kwargs):
+        """
+        :param command: RPC Command to execute
+        :param args: Positional arguments
+        :param kwargs: Keyword arguments
+        :return: Response from driver or empty dictionary if no response
+        """
+        socket = self._connect()
+        with socket:
+            log.debug('%r _command(%r %r %r)', self, command, args, kwargs)
+            timeout = kwargs.pop('timeout')
+            timeout = timeout if timeout is not None else 1000
+            msg = {'cmd': command, 'args': args, 'kwargs': kwargs}
+            socket.send_json(msg)
+            events = socket.poll(timeout=timeout)
+            if events:
+                response = socket.recv_json()
+                log.debug('%r _command RESPONSE: %r', self, response)
+                return response
+            raise TimeoutException({'timeout': 'no response in timeout interval %d' % timeout})
+
+    def ping(self, *args, **kwargs):
+        return self._command('process_echo', *args, **kwargs)
+
+    def execute(self, command, *args, **kwargs):
+        return self._command('execute_resource', command, *args, **kwargs)
+
+    def init_params(self, *args, **kwargs):
+        return self._command('set_init_params', *args, **kwargs)
+
+    def shutdown(self, *args, **kwargs):
+        return self._command('stop_driver_process', *args, **kwargs)
+
+    def get_state(self, *args, **kwargs):
+        return self._command('overall_state', *args, **kwargs)
+
+    def get_resource(self, *args, **kwargs):
+        return self._command('get_resource', *args, **kwargs)
+
+    def set_resource(self, resource, *args, **kwargs):
+        return self._command('set_resource', resource, *args, **kwargs)
+
+    def discover(self, *args, **kwargs):
+        return self._command('discover_state', *args, **kwargs)
+
+    def set_log_level(self, *args, **kwargs):
+        return self._command('set_log_level', *args, **kwargs)
+
+    def __repr__(self):
+        return 'ZmqDriverClient(%r, %r)' % (self.host, self.port)
---
+++ ooi_instrument_agent/test/test_utils.py
@@ -0,0 +1,175 @@
+import json
+import unittest
+
+import mock
+from consul import Consul
+from werkzeug.exceptions import NotFound
+
+from ooi_instrument_agent.client import ZmqDriverClient
+from ooi_instrument_agent.test.responses import health_response, port_agent_response
+from ooi_instrument_agent.utils import (list_drivers, get_client, get_host_and_port, get_service_host_and_port,
+                                        get_port_agent, get_from_request, get_timeout)
+
+
+class UtilsTest(unittest.TestCase):
+    def test_consul(self):
+        consul = Consul()
+        print consul.health.service('consul')
+
+    def test_list_drivers(self):
+        # mock the response from Consul
+        consul_mock = mock.Mock()
+        consul_mock.health.service.return_value = (1, json.loads(health_response))
+
+        result = list_drivers(consul_mock)
+        self.assertEqual(result, ["RS10ENGC-XX00X-00-SPKIRA001", "RS10ENGC-XX00X-00-TMPSFA001"])
+
+    def test_list_drivers_empty(self):
+        # mock the response from Consul
+        consul_mock = mock.Mock()
+        consul_mock.health.service.return_value = (1, [])
+
+        result = list_drivers(consul_mock)
+        self.assertEqual(result, [])
+
+    def test_get_client(self):
+        # mock the response from Consul
+        consul_mock = mock.Mock()
+        consul_mock.health.service.return_value = (1, json.loads(health_response))
+
+        result = get_client(consul_mock, 'test_driver_id')
+        self.assertIsInstance(result, ZmqDriverClient)
+
+    def test_get_client_missing(self):
+        # mock the response from Consul
+        consul_mock = mock.Mock()
+        consul_mock.health.service.return_value = (1, {})
+
+        with self.assertRaises(NotFound):
+            get_client(consul_mock, 'test_driver_id')
+
+    def test_get_host_and_port(self):
+        # mock the response from Consul
+        consul_mock = mock.Mock()
+        consul_mock.health.service.return_value = (1, json.loads(health_response))
+
+        host_and_port = get_host_and_port(consul_mock, 'test_driver_id')
+        self.assertEqual(host_and_port, (u'128.6.240.39', 42558))
+
+    def test_get_host_and_port_missing(self):
+        # mock the response from Consul
+        consul_mock = mock.Mock()
+        consul_mock.health.service.return_value = (1, {})
+
+        with self.assertRaises(NotFound):
+            get_host_and_port(consul_mock, 'test_driver_id')
+
+    def test_get_service_host_and_port(self):
+        # mock the response from Consul
+        consul_mock = mock.Mock()
+        consul_mock.health.service.return_value = (1, json.loads(health_response))
+
+        host_and_port = get_service_host_and_port(consul_mock, 'instrument_driver', tag='test_driver_id')
+        self.assertEqual(host_and_port, (u'128.6.240.39', 42558))
+
+    def test_get_service_host_and_port_missing(self):
+        # mock the response from Consul
+        consul_mock = mock.Mock()
+        consul_mock.health.service.return_value = (1, {})
+
+        response = get_service_host_and_port(consul_mock, 'instrument_driver', tag='test_driver_id')
+        self.assertIsNone(response)
+
+    def test_get_port_agent(self):
+        self.maxDiff = None
+        # mock the response from Consul
+        consul_mock = mock.Mock()
+        consul_mock.health.service.return_value = (1, json.loads(port_agent_response))
+
+        pa_dict = get_port_agent(consul_mock, 'test_driver_id')
+        self.assertEqual(pa_dict, {'command': {'host': u'128.6.240.39', 'port': 41347},
+                                   'data': {'host': u'128.6.240.39', 'port': 41347},
+                                   'sniff': {'host': u'128.6.240.39', 'port': 41347},
+                                   'da': {'host': u'128.6.240.39', 'port': 41347}})
+
+    def test_get_port_agent_missing(self):
+        # mock the response from Consul
+        consul_mock = mock.Mock()
+        consul_mock.health.service.return_value = (1, {})
+
+        with self.assertRaises(NotFound):
+            get_port_agent(consul_mock, 'test_driver_id')
+
+    # GET FROM REQUEST
+    @mock.patch('ooi_instrument_agent.utils.request')
+    def test_get_from_request_args(self, mocked):
+        mocked.args = {'test': 5}
+        mocked.form = {}
+        mocked.json = {}
+        val = get_from_request('test', 1)
+        self.assertEqual(val, 5)
+
+    @mock.patch('ooi_instrument_agent.utils.request')
+    def test_get_from_request_form(self, mocked):
+        mocked.args = {}
+        mocked.form = {'test': 5}
+        mocked.json = {}
+        val = get_from_request('test', 1)
+        self.assertEqual(val, 5)
+
+    @mock.patch('ooi_instrument_agent.utils.request')
+    def test_get_from_request_json(self, mocked):
+        mocked.args = {}
+        mocked.form = {}
+        mocked.json = {'test': 5}
+        val = get_from_request('test', 1)
+        self.assertEqual(val, 5)
+
+    @mock.patch('ooi_instrument_agent.utils.request')
+    def test_get_from_request_default(self, mocked):
+        mocked.args = {}
+        mocked.form = {}
+        mocked.json = {}
+        val = get_from_request('test', 1)
+        self.assertEqual(val, 1)
+
+    # GET TIMEOUT
+    @mock.patch('ooi_instrument_agent.utils.request')
+    def test_get_timeout_args(self, mocked):
+        mocked.args = {'timeout': 5}
+        mocked.form = {}
+        mocked.json = {}
+        val = get_timeout()
+        self.assertEqual(val, 5)
+
+    @mock.patch('ooi_instrument_agent.utils.request')
+    def test_get_timeout_form(self, mocked):
+        mocked.args = {}
+        mocked.form = {'timeout': 5}
+        mocked.json = {}
+        val = get_timeout()
+        self.assertEqual(val, 5)
+
+    @mock.patch('ooi_instrument_agent.utils.request')
+    def test_get_timeout_json(self, mocked):
+        mocked.args = {}
+        mocked.form = {}
+        mocked.json = {'timeout': 5}
+        val = get_timeout()
+        self.assertEqual(val, 5)
+
+    @mock.patch('ooi_instrument_agent.utils.request')
+    def test_get_timeout_default(self, mocked):
+        mocked.args = {}
+        mocked.form = {}
+        mocked.json = {}
+        val = get_timeout()
+        self.assertEqual(val, 90000)
+
+    @mock.patch('ooi_instrument_agent.utils.request')
+    def test_get_timeout_invalid(self, mocked):
+        mocked.args = {}
+        mocked.form = {}
+        mocked.json = {'timeout': 'bob'}
+        val = get_timeout()
+        self.assertEqual(val, 90000)
