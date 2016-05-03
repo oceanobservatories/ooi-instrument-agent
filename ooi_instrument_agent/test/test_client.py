42154c89fb561f8af6c96e25874a83f284e18510
Peter Cable <petercable@gmail.com>
2016-05-03 09:20:01 -0700

    Initial commit
    Change-Id: Ia446de07039e13e2dfae6cc4c8e4c61881c459d6

---
+++ ooi_instrument_agent/test/test_client.py
@@ -0,0 +1,79 @@
+import unittest
+import mock
+import zmq
+
+from ooi_instrument_agent.client import ZmqDriverClient, TimeoutException
+
+
+class ClientTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    @staticmethod
+    def assert_rpc_call(mock_socket, method, command, args, kwargs, timeout=1000, poll=True):
+        instance = mock_socket.return_value
+        instance.poll.return_value = poll
+
+        client = ZmqDriverClient(None, None)
+        client_method = getattr(client, method, None)
+        if client_method is None:
+            raise KeyError
+
+        client_method(*args, timeout=timeout, **kwargs)
+
+        instance.connect.assert_called_once_with('tcp://None:None')
+        instance.poll.assert_called_once_with(timeout=timeout)
+
+        expected_command = {
+            'cmd': command,
+            'args': args,
+            'kwargs': kwargs
+        }
+
+        instance.send_json.assert_called_once_with(expected_command)
+
+    @mock.patch('ooi_instrument_agent.client.zmq._Context._socket_class', autospec=zmq.sugar.Socket)
+    def test_ping(self, mocked_socket):
+        self.assert_rpc_call(mocked_socket, 'ping', 'process_echo', tuple(), {})
+
+    @mock.patch('ooi_instrument_agent.client.zmq._Context._socket_class', autospec=zmq.sugar.Socket)
+    def test_execute(self, mocked_socket):
+        self.assert_rpc_call(mocked_socket, 'execute', 'execute_resource',
+                             ('DRIVER_EVENT_START_AUTOSAMPLE',), {}, timeout=90000)
+
+    @mock.patch('ooi_instrument_agent.client.zmq._Context._socket_class', autospec=zmq.sugar.Socket)
+    def test_no_response(self, mocked_socket):
+        with self.assertRaises(TimeoutException):
+            self.assert_rpc_call(mocked_socket, 'ping', 'process_echo', tuple(), {}, poll=False)
+
+    @mock.patch('ooi_instrument_agent.client.zmq._Context._socket_class', autospec=zmq.sugar.Socket)
+    def test_init_params(self, mocked_socket):
+        self.assert_rpc_call(mocked_socket, 'init_params', 'set_init_params', ({'parameters': {}},), {})
+
+    @mock.patch('ooi_instrument_agent.client.zmq._Context._socket_class', autospec=zmq.sugar.Socket)
+    def test_shutdown(self, mocked_socket):
+        self.assert_rpc_call(mocked_socket, 'shutdown', 'stop_driver_process', tuple(), {})
+
+    @mock.patch('ooi_instrument_agent.client.zmq._Context._socket_class', autospec=zmq.sugar.Socket)
+    def test_get_state(self, mocked_socket):
+        self.assert_rpc_call(mocked_socket, 'get_state', 'overall_state', tuple(), {})
+
+    @mock.patch('ooi_instrument_agent.client.zmq._Context._socket_class', autospec=zmq.sugar.Socket)
+    def test_get_resource(self, mocked_socket):
+        self.assert_rpc_call(mocked_socket, 'get_resource', 'get_resource', ('DRIVER_PARAMETER_ALL',), {})
+
+    @mock.patch('ooi_instrument_agent.client.zmq._Context._socket_class', autospec=zmq.sugar.Socket)
+    def test_set_resource(self, mocked_socket):
+        self.assert_rpc_call(mocked_socket, 'set_resource', 'set_resource', ({'param': 'value'},), {})
+
+    @mock.patch('ooi_instrument_agent.client.zmq._Context._socket_class', autospec=zmq.sugar.Socket)
+    def test_get_discover(self, mocked_socket):
+        self.assert_rpc_call(mocked_socket, 'discover', 'discover_state', tuple(), {})
+
+    @mock.patch('ooi_instrument_agent.client.zmq._Context._socket_class', autospec=zmq.sugar.Socket)
+    def test_set_log_level(self, mocked_socket):
+        self.assert_rpc_call(mocked_socket, 'set_log_level', 'set_log_level', ('debug',), {})
+
+    def test_client_repr(self):
+        client = ZmqDriverClient(None, None)
+        self.assertEqual(repr(client), 'ZmqDriverClient(None, None)')
---
+++ ooi_instrument_agent/utils.py
@@ -0,0 +1,128 @@
+import json
+import logging
+
+from flask import request
+from werkzeug.exceptions import abort
+
+from ooi_instrument_agent.client import ZmqDriverClient
+
+
+DEFAULT_TIMEOUT = 90000
+log = logging.getLogger(__name__)
+
+
+def get_client(consul, driver_id):
+    """
+    Create a ZmqDriverClient for the specified driver_id
+    :param consul: Instance of consul.Consul
+    :param driver_id: Reference designator of target driver
+    :return: ZmqDriverClient if found, otherwise 404
+    """
+    return ZmqDriverClient(*get_host_and_port(consul, driver_id))
+
+
+def get_host_and_port(consul, driver_id):
+    """
+    Return the host and port for the specified driver_id
+    :param consul: Instance of consul.Consul
+    :param driver_id: Reference designator of target driver
+    :return: host, port if found, otherwise 404
+    """
+    host_and_port = get_service_host_and_port(consul, 'instrument_driver', tag=driver_id)
+    if host_and_port is None:
+        abort(404)
+    return host_and_port
+
+
+def get_service_host_and_port(consul, service_id, tag=None):
+    """
+    Return the first passing host and port for the specified service_id
+    :param consul: Instance of consul.Consul
+    :param service_id: service_id
+    :param tag: tag
+    :return: host, port if found, otherwise None
+    """
+    index, matches = consul.health.service(service_id, tag=tag, passing=True)
+    for match in matches:
+        host = match.get('Node', {}).get('Address')
+        port = match.get('Service', {}).get('Port')
+        if host and port:
+            return host, port
+
+
+def list_drivers(consul):
+    """
+    Return a list of all passing drivers currently registered in Consul
+    :param consul: Instance of consul.Consul
+    :return: List of reference designators
+    """
+    drivers = []
+    index, passing = consul.health.service('instrument_driver', passing=True)
+    for each in passing:
+        tags = each.get('Service', {}).get('Tags', [])
+        drivers.extend(tags)
+    return drivers
+
+
+def get_port_agent(consul, driver_id):
+    """
+    Fetch the port agent information for the specified driver from Consul
+    :param consul: Instance of consul.Consul
+    :param driver_id: Reference designator of target driver
+    :return: Dictionary containing the port agent data for the specified driver
+    """
+    return_dict = {}
+    for name, service_id in [('data', 'port-agent'),
+                             ('command', 'command-port-agent'),
+                             ('sniff', 'sniff-port-agent'),
+                             ('da', 'da-port-agent')]:
+        host_and_port = get_service_host_and_port(consul, service_id, tag=driver_id)
+        if host_and_port:
+            host, port = host_and_port
+            return_dict[name] = {'host': host, 'port': port}
+    if return_dict:
+        return return_dict
+    abort(404)
+
+
+def get_from_request(name, default=None):
+    """
+    Extract the target parameter from a Flask request object. Attempts to do the right
+    thing whether the input data was passed as URL query params, a form or as JSON.
+    :param name: Target parameter
+    :param default: Default value to return if not found
+    :return: Extracted value if found, else default
+    """
+
+    def extract(value_dict, name):
+        val = value_dict.get(name)
+        if val is None:
+            return default
+        try:
+            val = json.loads(val)
+        except (TypeError, ValueError):
+            pass
+        return val
+
+    if request.args:
+        return extract(request.args, name)
+    if request.form:
+        return extract(request.form, name)
+
+    if request.json:
+        return request.json.get(name, default)
+
+    return default
+
+
+def get_timeout():
+    """
+    Get the timeout from the request object as an int
+    :return: timeout
+    """
+    val = get_from_request('timeout')
+
+    try:
+        return int(val)
+    except (ValueError, TypeError):
+        return DEFAULT_TIMEOUT
