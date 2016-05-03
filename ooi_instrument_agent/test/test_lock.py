7dd287a7de137ab10009501c8ded11deba9ecbcb
Peter Cable <petercable@gmail.com>
2016-05-03 09:20:01 -0700

    Initial commit
    Change-Id: Ia446de07039e13e2dfae6cc4c8e4c61881c459d6

---
+++ ooi_instrument_agent/test/test_lock.py
@@ -0,0 +1,42 @@
+import unittest
+
+import consul
+
+from ooi_instrument_agent.lock import LockManager, Locked
+
+
+prefix = 'unittest/locks'
+
+test_locks = {
+    'driver_one': 'one',
+    'driver_two': 'two'
+}
+
+
+# These unit tests require a running Consul instance
+class LockTest(unittest.TestCase):
+    def setUp(self):
+        self.consul = consul.Consul()
+        self.consul.kv.delete(prefix, recurse=True)
+
+    def test_iter(self):
+        lm = LockManager(self.consul, prefix=prefix)
+        lm.update(test_locks)
+        self.assertDictEqual(dict(lm.iteritems()), test_locks)
+
+    def test_lock(self):
+        lm = LockManager(self.consul, prefix=prefix)
+        lock_id = 'lock_me'
+        locker = 'test'
+        lm[lock_id] = locker
+
+        # locking again with the same id should raise Locked
+        with self.assertRaises(Locked):
+            lm[lock_id] = locker
+
+        # attempt to lock with a different ID
+        with self.assertRaises(Locked):
+            lm[lock_id] = 'fail'
+
+        # remove the lock
+        del lm[lock_id]
